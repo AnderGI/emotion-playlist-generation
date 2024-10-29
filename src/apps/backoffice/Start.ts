bb3d652fc0746017547416831ab4608f3d9eb565
AnderGI <agibarguen@gmail.com>
2024-10-29 20:01:52 +0100

    Refactor. start ts

--- src/apps/backoffice/Start.ts
+++ src/apps/backoffice/Start.ts
@@ -1,8 +1,7 @@
-/* import { BackofficeBackendApp } from "./BackofficeBackendApp.js";
+import { BackofficeBackendApp } from "./BackofficeBackendApp.js";
 
 try{
   (new BackofficeBackendApp()).start()
 } catch {
   process.exit(1)
 }
- */
