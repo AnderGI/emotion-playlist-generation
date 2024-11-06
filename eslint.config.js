a06df94f617fdf4d8e9d0b8ccc4a79eea5e772ab
AnderGI <agibarguen@gmail.com>
2024-11-06 09:06:03 +0100

    GitHub Actions Linting bugfix

--- eslint.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {rules: {
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-empty-object-type': 'off'
-  }}
-];
